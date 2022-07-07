fc012e2d566b0b24ab14123c3cf47c3ad9d2015a
Andrey Antonowycz <andrey@antonowycz.com>
2022-07-07 19:08:35 +0200

    Add sql for jumpstarting database

---
+++ jumpstart-database.sql
@@ -0,0 +1,43 @@
+-- MySQL dump 10.13  Distrib 5.7.37, for Linux (x86_64)
+--
+-- Host: localhost    Database: watmoetikhalen
+-- ------------------------------------------------------
+-- Server version	5.7.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `watmoetikhalen`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `watmoetikhalen` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `watmoetikhalen`;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `user_password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `user_email` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `user_status` int(11) NOT NULL,
+  `user_activation` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_name` (`user_name`,`user_password_hash`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
